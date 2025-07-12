3970122721a5a42211d080456d296c5fb0f0c946
Ryan Kim <ryanhkim0418@gmail.com>
2025-07-12 16:38:55 -0500

    ?

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
+<<<<<<< HEAD
 let cells = document.querySelectorAll(".cell");
 let restartGameButton = document.querySelector(".restart-btn");
 
 let board = ["", "", "", "", "", "", "", "", ""];
 let currentPlayer = "X";
 let gameActive = true;
+=======
+function Gameboard(){
+    const board = [];
+
+    for(let i = 0;i < 3; i++){
+        board[i]=[];
+        for(let j = 0; j < 3; j++){
+            board[i].push(Cell());
+        }
+    }
+>>>>>>> parent of 12273ea (Created basic logic/ trying to get winner logic to work in console first)
 
 let winningConditions = [
   [0, 1, 2],
@@ -16,6 +28,7 @@ let winningConditions = [
   [2, 4, 6],
 ];
 
+<<<<<<< HEAD
 cells.forEach((cell) => {
   cell.addEventListener("click", cellClick);
 });
@@ -55,6 +68,28 @@ function checkResult() {
       break;
     }
   }
+=======
+    const printBoard = () => {
+        const boardWithCellValues = board.map((row) => row.map((cell) => cell.getValue()));
+        console.log(boardWithCellValues);
+    }
+
+    const chooseCell = (row, column, player) => {
+
+        console.log("Selected row: " + row + " column: " + column);
+        board[row][column] = player;
+    }
+
+    return { getBoard, chooseCell, printBoard };
+}
+
+function Cell() {
+    let value = 0;
+
+    const addToken = (player) => {
+        value = player;
+    };
+>>>>>>> parent of 12273ea (Created basic logic/ trying to get winner logic to work in console first)
 
   if (roundWon) {
     gameActive = false;
@@ -64,6 +99,7 @@ function checkResult() {
     return;
   }
 
+<<<<<<< HEAD
   if (!board.includes("")) {
     gameActive = false;
     document.querySelector(".player-turn").textContent = "It's a draw!";
@@ -87,3 +123,66 @@ function restartGame() {
 
   document.querySelector(".player-turn").textContent = "Player X's turn";
 }
+=======
+    return {addToken, getValue};
+}
+
+function GameController(
+    playerOneName = "Player One", playerTwoName = "Player Two"
+) {
+    const board = Gameboard();
+    const players = [
+        {
+            name: playerOneName,
+            token: 1
+        },
+        {
+            name: playerTwoName,
+            token: 2
+        }
+    ];
+
+    let activePlayer = players[0];
+
+    const switchPlayerTurn = () => {
+        activePlayer = acticePlayer === players[0] ? players[1] : players[0];
+    };
+
+    const getActivePlayer = () => activePlayer;
+
+    const printNewRound = () => {
+        board.printBoard();
+        console.log("${getActivePlayer().name}'s turn.");
+    };
+
+    const playRound = (row, column) => {
+        console.log("Placing ${getActivePlayer().name}'s token into row ${row} and column ${column}...");
+        board.chooseCell(row, column, getActivePlayer().token);
+
+        /*Check win condition*/
+        /*Check rows*/
+
+        /*Check columns*/
+
+        /*Check diagonal*/
+        if(board.getBoard()[0][0] == board.getBoard()[1][1] &&
+            board.getBoard()[1][1] == board.getBoard()[2][2] &&
+            board.getBoard()[0][0] == board.getBoard()[2][2]
+        ) {
+            console.log("WIN");
+        }
+
+        switchPlayerTurn();
+        printNewRound();
+    };
+
+    printNewRound();
+
+    return {
+        playRound,
+        getActivePlayer
+    };
+}
+
+const game = GameController();
+>>>>>>> parent of 12273ea (Created basic logic/ trying to get winner logic to work in console first)
